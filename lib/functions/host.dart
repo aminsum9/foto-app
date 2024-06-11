a81135e0717f83ef0f6b216eb9595c5633118038
amin subagiyo <aminsum9@gmail.com>
2024-06-12 01:34:26 +0700

    add host prod

--- lib/functions/host.dart
+++ lib/functions/host.dart
@@ -1,6 +1,7 @@
 library my_prj.globals;
 
 // ignore: non_constant_identifier_names
-String BASE_URL = 'http://192.168.140.128:8000/api/';
+// String BASE_URL = 'http://192.168.140.128:8000/api/';
+String BASE_URL = 'http://93.127.217.176/api/';
 // ignore: non_constant_identifier_names
-String BASE_URL_IMG = 'http://192.168.140.128:8000/';
+String BASE_URL_IMG = 'http://93.127.217.176/';
